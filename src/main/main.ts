c917dfcfa02102d3a09b162e0635b73f7d2e6c8c
Vitaly Alexeev <jameshudsonrandall@gmail.com>
2025-03-28 23:59:34 +0300

    fix path bug

--- src/main/main.ts
+++ src/main/main.ts
@@ -20,7 +20,7 @@ function createWindow() {
     mainWindow.loadURL("http://localhost:5173");
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, "../../dist/index.html"));
+    mainWindow.loadFile(path.join(__dirname, "../../dist/renderer/index.html"));
   }
 
   mainWindow.on("closed", () => {
